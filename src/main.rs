9ed2043f3477260ea40728ef90f5e0393f10b943
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2024-02-20 08:47:05 +1300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use text_io::read;
 
 fn main() {
     print!("Reading file...\r");
-    print!("Is this file local (1) or on the web (2)?\r");
-    let input: String = read!("{}\n");
+    print!("Is this file local (1) or on the web (2)?\r\n\r");
+    let input: String = read!();
+    println!("{input}.");
     let input_: i32 = input.parse::<i32>().unwrap();
-    println!("{input_}");
+    println!("{input_}!");
 }
