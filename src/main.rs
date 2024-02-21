f88cd29a2f231bbd9d0e5f0dead7febc3a22a21f
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2024-02-21 14:27:51 +1300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use text_io::read;
 
 fn main() {
-    print!("Reading file...\r");
     print!("Is this file local (1) or on the web (2)?\r\n\r");
-    let input: String = read!();
-    println!("{input}.");
-    let input_: i32 = input.parse::<i32>().unwrap();
-    println!("{input_}!");
+    let input_: String = read!();
+    let lw: i32 = input_.parse::<i32>().unwrap();
+    print!("{lw} chosen...\r");
+
+    print!("Please enter the relevant address or location of the file:\n\r\n");
+    let location: String = read!(); 
+    print!("{location} chosen...\r");
+    
+    print!("Reading file...\r");
 }
