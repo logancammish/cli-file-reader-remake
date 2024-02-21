e9eed88f3b09446cab1d80d6838e2e43c4d66954
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2024-02-21 14:37:33 +1300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+use std::{thread, time};
+
 use text_io::read;
 
 fn main() {
     print!("Is this file local (1) or on the web (2)?\r\n\r");
     let input_: String = read!();
     let lw: i32 = input_.parse::<i32>().unwrap();
+    thread::sleep(time::Duration::from_millis(1000));
     print!("{lw} chosen...\r");
+    thread::sleep(time::Duration::from_millis(1000));
 
     print!("Please enter the relevant address or location of the file:\n\r\n");
     let location: String = read!(); 
+    thread::sleep(time::Duration::from_millis(1000));
     print!("{location} chosen...\r");
+    thread::sleep(time::Duration::from_millis(1000));
     
     print!("Reading file...\r");
 }
