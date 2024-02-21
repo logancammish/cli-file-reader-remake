7ca560d536b24f28b6867fe8054763e062253a9e
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2024-02-22 11:39:07 +1300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,4 +12,5 @@
 echo This script is untested currently
 cd ~/bin
 wget -O ~/bin/reader "https://github.com/logancammish/cli-file-reader-remake/releases/download/0.0.1-hf2/reader"
+chmod -x reader
 export PATH=$PATH:~/bin
