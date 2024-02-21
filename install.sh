da30d660323efcf5bd85b893302392aef06da552
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2024-02-22 11:16:22 +1300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+################################################
+# ! Untested currently ! 
+# you may have to make modifications
+# ! Untested currently !
+################################################
+# License: GNU General Public License v3.0
+# Author: Logan Cammish
+################################################
+
+echo This script is untested currently
+cd ~/bin
+wget -O ~/bin/reader "https://github.com/logancammish/cli-file-reader-remake/releases/download/0.0.1-hf2/reader"
+export PATH=$PATH:~/bin
