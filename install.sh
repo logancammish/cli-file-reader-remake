73ae5bc442185bc8052a1d44b80ef01f1190cdbd
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2024-02-22 17:09:40 +1300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@
 
 echo Please ensure you have a bin directory before running
 cd ~/bin
-wget -O ~/bin/reader "https://github.com/logancammish/cli-file-reader-remake/releases/download/0.0.1-hf2/reader"
+wget -O ~/bin/reader "https://github.com/logancammish/cli-file-reader-remake/releases/download/0.0.2/reader"
 chmod -x reader
 export PATH=$PATH:~/bin
