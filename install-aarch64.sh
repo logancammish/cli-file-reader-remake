8662b5e2bce42a1cce6a4d8434b6e6e980f10f2f
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2024-02-23 08:40:47 +1300

    Update install-aarch64.sh

--- install-aarch64.sh
+++ install-aarch64.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-################################################
-# License: GNU General Public License v3.0
-# Author: Logan Cammish
-################################################
-
 echo Please ensure you have a bin directory before running
 cd ~/bin
 wget -O ~/bin/reader "https://github.com/logancammish/cli-file-reader-remake/releases/download/0.0.2/reader-aarch64"
