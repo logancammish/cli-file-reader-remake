aa1ffd31c00bb61bc0f87a6fbf9c22973b9ca87c
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2024-02-29 17:09:41 +1300

    Delete install-aarch64.sh

--- install-aarch64.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo Please ensure you have a bin directory before running
-cd ~/bin
-wget -O ~/bin/reader "https://github.com/logancammish/cli-file-reader-remake/releases/download/0.0.2/reader-aarch64"
-chmod -x reader
-export PATH=$PATH:~/bin
