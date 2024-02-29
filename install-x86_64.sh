7c70fd14116449eea7abab3b51296bdc80e2f3b0
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2024-02-29 18:10:58 +1300

    Check if bin directory exists

--- install-x86_64.sh
+++ install-x86_64.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo Please ensure you have a bin directory before running
+if [ -d ~/bin ]; then
+  echo "bin directory exists, continuing..."
+else 
+  echo "bin directory does not exist, creating..."
+  mkdir ~/bin
+  echo "continuing..."
+fi
+
 cd ~/bin
 wget -O ~/bin/reader "https://github.com/logancammish/cli-file-reader-remake/releases/download/0.0.2/reader"
 sudo chmod -x reader
